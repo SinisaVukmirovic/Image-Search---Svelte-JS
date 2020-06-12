4b6db3d2da597f6a6249881968e9318879f89d6e
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-06-12 20:17:38 +0200

    Initial commit with basic setup

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="app">
+	<h1>Svelte Image Search</h1>
+	<h4>with Nature Images API</h4>
+
+	<form>
+		<label for="search-term">Search:</label>
+		<input type="text" name="search-term" />
+		<button type="submit">Search</button>
+	</form>
+
+	<img id="searchingImg" src="../searching.gif" alt="Searching...">
+
+	<section class="images"></section>
+</div>
